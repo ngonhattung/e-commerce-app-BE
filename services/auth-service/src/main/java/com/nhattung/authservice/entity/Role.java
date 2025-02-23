5167c4bd5ea485f49d2b2134074315764a20a54d
ngonhattung <ngonhattung08062003@gmail.com>
2025-02-23 22:00:52 +0700

    setup entity

---
+++ services/auth-service/src/main/java/com/nhattung/authservice/entity/Role.java
@@ -0,0 +1,28 @@
+package com.nhattung.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users = new HashSet<>();
+}
---
+++ services/auth-service/src/main/java/com/nhattung/authservice/entity/User.java
@@ -0,0 +1,40 @@
+package com.nhattung.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.HashSet;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade =
+            {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles = new HashSet<>();
+
+}
