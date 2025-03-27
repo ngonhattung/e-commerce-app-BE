807656dbc7bfca2d4a75f4cf9272b6a6ad0af0c0
ngonhattung <ngonhattung08062003@gmail.com>
2025-03-27 13:03:10 +0700

    config security for inventory service

---
+++ services/inventory_service/src/main/java/com/nhattung/inventory_service/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.nhattung.inventory_service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhattung.inventory_service.exception.ErrorCode;
+import com.nhattung.inventory_service.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+
+        response.setStatus(errorCode.getStatusCode().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
---
+++ services/inventory_service/src/main/java/com/nhattung/inventory_service/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.nhattung.inventory_service.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@Slf4j
+public class SecurityConfig {
+
+    private final String[] PUBLIC_ENDPOINTS = {
+            "/products/**",
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        log.info("Public endpoints: {}", List.of(PUBLIC_ENDPOINTS));
+        httpSecurity.
+                authorizeHttpRequests(request -> request.
+                        requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                .anyRequest()
+                .authenticated());
+
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
+                .authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+
+        return httpSecurity.build();
+    }
+
+
+
+}
