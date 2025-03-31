7a244021a2d0904b818d8b3d3e2beb7937c855e9
ngonhattung <ngonhattung08062003@gmail.com>
2025-03-31 14:53:28 +0700

    setup entity layer for cart service

---
+++ services/cart-service/src/main/java/com/nhattung/cartservice/entity/CartItem.java
@@ -0,0 +1,35 @@
+package com.nhattung.cartservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+    private Long productId;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public void setTotalPrice() {
+        this.totalPrice = this.unitPrice.multiply(BigDecimal.valueOf(quantity));
+    }
+}
