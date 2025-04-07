069d6cf88140d5a9fce70b0b3e15c724ee008450
ngonhattung <ngonhattung08062003@gmail.com>
2025-04-07 13:54:49 +0700

    setup entity layer for promotion service

---
+++ services/promotion-service/src/main/java/com/nhattung/promotionservice/entity/Promotion.java
@@ -0,0 +1,40 @@
+package com.nhattung.promotionservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String promotionName;
+    private String description;
+    private String couponCode;
+    private BigDecimal discountPercent;
+    private BigDecimal discountAmount;
+    private BigDecimal minimumOrderValue;
+    private Instant startDate;
+    private Instant endDate;
+    private Boolean isActive;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
