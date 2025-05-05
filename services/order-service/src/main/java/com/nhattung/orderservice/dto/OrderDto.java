d6081de14a430fdc9cd74f78ef3fd6f152a11dfc
ngonhattung <ngonhattung08062003@gmail.com>
2025-05-05 11:53:44 +0700

    fix change orderdto localdate to localdatetime

--- services/order-service/src/main/java/com/nhattung/orderservice/dto/OrderDto.java
+++ services/order-service/src/main/java/com/nhattung/orderservice/dto/OrderDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -16,7 +17,7 @@ import java.util.List;
 public class OrderDto {
     private String id;
     private String userId;
-    private LocalDate orderDate;
+    private LocalDateTime orderDate;
     private BigDecimal totalAmount;;
     private String status;
     private String shippingAddress;
